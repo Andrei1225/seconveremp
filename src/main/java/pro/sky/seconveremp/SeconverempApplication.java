796b999fb8601c2e3ea53d14b72d06d0126e2743
Андрей <andybro17@yandex.ru>
2022-08-25 15:58:57 +0300

    Initial commit

---
+++ src/main/java/pro/sky/seconveremp/SeconverempApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.seconveremp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeconverempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeconverempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/seconveremp/SeconverempApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.seconveremp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeconverempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
